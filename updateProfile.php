f29f4d81baa9036b9ebfe1bdb67faae6ed68b467
Ng Thian Ping <s10247447@connect.np.edu.sg>
2024-02-04 18:30:34 +0800

    email validation for update profile

--- updateProfile.php
+++ updateProfile.php
@@ -5,7 +5,8 @@
 </head>
 
 <?php
-
+// Include the PHP file that establishes database connection handle: $conn
+include_once("mysql_conn.php");
 //Read the data input from previous page
 $name = $_POST["name"];
 $address = $_POST["address"];
@@ -15,27 +16,41 @@ $email = $_POST["email"];
 $password = $_POST["password"];
 $shopperId = $_SESSION["ShopperID"];
 
-// Include the PHP file that establishes database connection handle: $conn
-include_once("mysql_conn.php");
-
-// Include the PHP file that establishes database connection handle: $conn
-include_once("mysql_conn.php");
-//Define the select statement for email where the email in the database record matches the email enterd by the shopper
-$qry = "UPDATE Shopper set Name=?, Address=?, Country=?, Phone=?, Email=?, Password=? where ShopperID=?";
+$qry = "SELECT Email from Shopper where Email=?";
 $stmt = $conn->prepare($qry);
-$stmt->bind_param("ssssssi",$name,$address,$country, $phone,$email,$password,$shopperId);
-
-if($stmt->execute()){
-    include("header.php");
-    echo "<br/>";
-    echo $Message = "<div id='update' class='col-sm-12'>Update succesful!<br />
-                    <div class='col-sm-1 update-btn'>
-                    <a class='btn btn-primary' href='index.php'>Back</a> 
-                    </div>";
+$stmt->bind_param("s",$email);
+$stmt->execute();
+$result = $stmt->get_result();
+if($row = $result->fetch_assoc()){
+    include('header.php');
+    if($row['Email'] === $email){
+        echo $Message = "<div class='container-fluid mt-4 validate-email text-center'>Existing email please try again
+                        <form action='Profile.php' method='post' class='container-fluid col-sm-3' id='validate-email-input'>
+                        <button class='btn btn-primary'>Click here to try again</button>
+                        </form>
+                        </div>";
+    }
     include("footer.php");
 }
+else{
+        //Define the select statement for email where the email in the database record matches the email enterd by the shopper
+        $qry = "UPDATE Shopper set Name=?, Address=?, Country=?, Phone=?, Email=?, Password=? where ShopperID=?";
+        $stmt = $conn->prepare($qry);
+        $stmt->bind_param("ssssssi",$name,$address,$country, $phone,$email,$password,$shopperId);
 
-$stmt->close();
-$conn->close();
+        if($stmt->execute()){
+            include("header.php");
+            echo "<br/>";
+            echo $Message = "<div id='update' class='col-sm-12'>Update succesful!<br />
+                            <div class='col-sm-1 update-btn'>
+                            <a class='btn btn-primary' href='index.php'>Back</a> 
+                            </div>";
+            include("footer.php");
+        }
+
+        $stmt->close();
+        $conn->close();
+        }
+
 
 ?>
